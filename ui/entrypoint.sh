4f136c03f2753e16a77037769c4b297823bbfddc
mirmika <meryemdemirbas@gmail.com>
2025-03-30 19:11:06 +0200

    Update config folder as configs

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$API_URL" ]; then
   echo "Replacing default API URL with $API_URL"
-  sed -i "s|http://basic3tier-api:5000/|$API_URL|g" /usr/share/nginx/html/config/config.json
+  sed -i "s|http://basic3tier-api:5000/|$API_URL|g" /usr/share/nginx/html/configs/config.json
 fi
 
 exec "$@"
